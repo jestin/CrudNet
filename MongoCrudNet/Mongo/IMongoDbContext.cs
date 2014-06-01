5e62fb60cd5ab19b656313e8e5688faa707033bd
Jestin Stoffel <jestin.stoffel@gmail.com>
2014-06-01 01:42:49 -0500

    added missing project files, some reference files, and some base mongo stuff

---
+++ MongoCrudNet/Mongo/IMongoDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using MongoDB.Driver;
+
+namespace MongoCrudNet.Mongo
+{
+    /// <summary>
+    /// This interface defines what is needed to provide MongoDB access to a
+    /// CRUD repository based on MongoDB.
+    /// </summary>
+    public interface IMongoDbContext
+    {
+        MongoServer Server { get; }
+        MongoDatabase Database { get; }
+        bool Initialized { get; }
+
+        /// <summary>
+        /// Connects to the server and the database
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <param name="databaseName"></param>
+        /// <returns></returns>
+        bool Connect(string connectionString, string databaseName);
+
+        /// <summary>
+        /// Registers a class for serialization
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        void RegisterClass<T>();
+
+        /// <summary>
+        /// Checks to see if a class is already registered with serialization
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        bool IsClassRegistered(Type type);
+    }
+}
---
+++ MongoCrudNet/Mongo/MongoDbContext.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+
+namespace MongoCrudNet.Mongo
+{
+    /// <summary>
+    /// This class provides CRUD repositories access to MongoDB.
+    /// </summary>
+    public class MongoDbContext : IMongoDbContext
+    {
+        /// <summary>
+        /// The MongoDB server to which this context connects
+        /// </summary>
+        public virtual MongoServer Server { get; set; }
+
+        /// <summary>
+        /// The MongoDB database to which this context connects
+        /// </summary>
+        public virtual MongoDatabase Database { get; set; }
+
+        /// <summary>
+        /// Whether or not the connection to the database has been initialized
+        /// </summary>
+        public virtual bool Initialized { get; set; }
+
+        /// <summary>
+        /// Connects to a specified server and database, with any parameters passed
+        /// in by the connection string
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <param name="databaseName"></param>
+        /// <returns></returns>
+        public virtual bool Connect(string connectionString, string databaseName)
+        {
+            Initialized = false;
+
+            var client = new MongoClient(connectionString);
+            Server = client.GetServer();
+
+            if (Server != null)
+            {
+                Database = Server.GetDatabase(databaseName);
+
+                if (Database != null)
+                {
+                    EnsureIndexes();
+                    Initialized = true;
+                }
+            }
+
+            return Initialized;
+        }
+
+        /// <summary>
+        /// This is a method that abstracts registering classes
+        /// into the BsonClassMap provided by the .NET MongoDB
+        /// driver.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public void RegisterClass<T>()
+        {
+            BsonClassMap.RegisterClassMap<T>();
+        }
+
+        /// <summary>
+        /// This is a way to check if a class has already be
+        /// registered with the BsonClassMap.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public bool IsClassRegistered(Type type)
+        {
+            return BsonClassMap.IsClassMapRegistered(type);
+        }
+
+        /// <summary>
+        /// Override this method to add any indexes you may want for your database
+        /// </summary>
+        protected virtual void EnsureIndexes()
+        {
+        }
+    }
+}
