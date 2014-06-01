0a89364a64ecadd19992419b4a6070aff6970136
Jestin Stoffel <jestin.stoffel@gmail.com>
2014-06-01 01:22:58 -0500

    added a project with CRUD interfaces

---
+++ MongoCrudNet/CRUD/IDeletable.cs
@@ -0,0 +1,36 @@
+﻿namespace MongoCrudNet.CRUD
+{
+    public interface IDeletable<T>
+    {
+        /// <summary>
+        /// This Delete method will delete an object that has a
+        /// specific complex key.
+        /// </summary>
+        /// <param name="keys"></param>
+        /// <returns></returns>
+        bool Delete(object[] keys);
+
+        /// <summary>
+        /// This Delete method will delete a given object from
+        /// the database.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        bool Delete(T item);
+
+        /// <summary>
+        /// This method will remove all objects of this type from
+        /// the database.
+        /// </summary>
+        /// <returns></returns>
+        bool DeleteAll();
+
+        /// <summary>
+        /// This DeleteAll method will delete all objects that have a
+        /// specific complex key.
+        /// </summary>
+        /// <param name="keys"></param>
+        /// <returns></returns>
+        bool DeleteAll(object[] keys);
+    }
+}
---
+++ MongoCrudNet/CRUD/IUpdatable.cs
@@ -0,0 +1,15 @@
+﻿namespace MongoCrudNet.CRUD
+{
+    public interface IUpdatable<T>
+    {
+        /// <summary>
+        /// This method updates the object in the database.  Be
+        /// careful, because some databases and ORMs require that
+        /// the object being updated was the actual one that was
+        /// retrieved from the database.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        T Update(T item);
+    }
+}
