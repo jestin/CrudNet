5bd36202e17b0a21966a8fffdfab8225482ded03
Jestin Stoffel <jestin.stoffel@gmail.com>
2014-06-14 15:33:41 -0500

    forgot to add some things after the refactor

---
+++ CrudNet/ICreatable.cs
@@ -0,0 +1,24 @@
+﻿namespace CrudNet
+{
+    public interface ICreatable<T>
+    {
+        /// <summary>
+        /// This version of Create is useful if you implement CRUD with an ORM framework
+        /// that must created the objects that get stored into the database(such as
+        /// Entity).  Because it also calls InitializeItem, it is recommended to create
+        /// you database objects with this method, even if your database doesn't require
+        /// it.
+        /// </summary>
+        /// <returns></returns>
+        T Create();
+
+        /// <summary>
+        /// This is the standard Create method that takes an object and inserts it into
+        /// the database.  Depending on how CRUD is implemented, this object may need
+        /// to be created with the other Create method.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        T Create(T item);
+    }
+}
---
+++ CrudNet/IRetrievable.cs
@@ -0,0 +1,22 @@
+﻿namespace CrudNet
+{
+    public interface IRetrievable<T>
+    {
+        /// <summary>
+        /// This Retrieve method is for the simple case where a
+        /// database object has only a single key.  It will be
+        /// the one that gets used in most cases.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        T Retrieve(object key);
+
+        /// <summary>
+        /// This Retrieve method can be useful for when the
+        /// database object has a complex key.
+        /// </summary>
+        /// <param name="keys"></param>
+        /// <returns></returns>
+        T Retrieve(object[] keys);
+    }
+}
