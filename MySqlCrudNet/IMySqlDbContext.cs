a87e6182a7c59b9d3a3b4ec2fc23259269376b24
Jestin Stoffel <jestin.stoffel@gmail.com>
2014-06-14 20:50:26 -0500

    added some comments

--- MySqlCrudNet/IMySqlDbContext.cs
+++ MySqlCrudNet/IMySqlDbContext.cs
@@ -2,17 +2,28 @@ using MySql.Data.MySqlClient;
 
 namespace MySqlCrudNet
 {
+	/// <summary>
+	/// Manages the connection to a MySql database
+	/// </summary>
 	public interface IMySqlDbContext
 	{
+		/// <summary>
+		/// The current connection to a database
+		/// </summary>
+		/// <value>The connection.</value>
 		MySqlConnection Connection { get; set; }
+
+		/// <summary>
+		/// A flag that indicates whether this context has been initialized
+		/// </summary>
+		/// <value><c>true</c> if initialized; otherwise, <c>false</c>.</value>
 		bool Initialized { get; }
 
 		/// <summary>
 		/// Connects to the server and the database
 		/// </summary>
 		/// <param name="connectionString"></param>
-		/// <param name="databaseName"></param>
-		/// <returns></returns>
+		/// <returns>True when the connection succeeds</returns>
 		bool Connect(string connectionString);
 
 		/// <summary>
--- MySqlCrudNet/MySqlDbContext.cs
+++ MySqlCrudNet/MySqlDbContext.cs
@@ -3,14 +3,30 @@ using MySql.Data.MySqlClient;
 
 namespace MySqlCrudNet
 {
+	/// <summary>
+	/// Manages the connection to a MySql database
+	/// </summary>
 	public class MySqlDbContext : IMySqlDbContext
 	{
 		#region IMySqlDbContext implementation
 
+		/// <summary>
+		/// The current connection to a database
+		/// </summary>
+		/// <value>The connection.</value>
 		public MySqlConnection Connection { get; set; }
 
+		/// <summary>
+		/// A flag that indicates whether this context has been initialized
+		/// </summary>
+		/// <value><c>true</c> if initialized; otherwise, <c>false</c>.</value>
 		public bool Initialized { get; set; }
 
+		/// <summary>
+		/// Connects to the server and the database
+		/// </summary>
+		/// <param name="connectionString"></param>
+		/// <returns>True when the connection succeeds</returns>
 		public bool Connect(string connectionString)
 		{
 			Initialized = false;
@@ -29,6 +45,9 @@ namespace MySqlCrudNet
 			return Initialized;
 		}
 
+		/// <summary>
+		/// Disconnects from the server and closes the connection
+		/// </summary>
 		public void Disconnect()
 		{
 			Connection.Close();
